6a8399fd701be088fd8467d6ad7de4545ce1a213
Andy Cotter <andy.cotter@gmail.com>
2023-06-21 20:38:26 -0700

    Added ngx bootstrap, added range calendar

--- src/app/tripAdd/trip-add.component.ts
+++ src/app/tripAdd/trip-add.component.ts
@@ -16,25 +16,24 @@ export class TripAddComponent {
   @Input() trips: ITrip[] = []; //Input the trips just to make sure there are no intersections
 
   errorMessage: string = '';
-  startDateString: string = '';
-  endDateString: string = '';
+  dateRange: Date[] = [];
 
   //methods
-  canAddTrip(startDateString: string, endDateString: string): boolean {
+  canAddTrip(dates: Date[]): boolean {
     this.errorMessage = ''; //reset error message
 
     //Check if there are values in start and end date
-    if (startDateString == '' || endDateString == '') return false;
+    if (dates[0] == undefined || dates[1] == undefined) return false;
 
+    const startDate: Date = dates[0];
+    const endDate: Date = dates[1];
     //Check if arrival is after departure (can't happen unless time machine)
-    if (startDateString > endDateString) {
+    if (startDate > endDate) {
       this.errorMessage = 'Departure date cannot be before arrival date';
       return false;
     }
 
     //Check if current dates intersect with another trip
-    let startDate: Date = new Date(startDateString);
-    let endDate: Date = new Date(endDateString);
     if (
       this.trips.filter(
         (trip: ITrip) =>
@@ -49,17 +48,16 @@ export class TripAddComponent {
     return true; //no issues can add trip
   }
 
-  addTrip(trip: ITrip, startDateString: string, endDateString: string): void {
+  addTrip(trip: ITrip, dates: Date[]): void {
     //converting the date in a string format to a Date format
-    trip.startDate = new Date(startDateString);
-    trip.endDate = new Date(endDateString);
+    trip.startDate = dates[0];
+    trip.endDate = dates[1];
 
     //Sending the date to the parent
     this.tripAdded.emit(trip);
 
     //Reseting the date
-    this.startDateString = '';
-    this.endDateString = '';
+    this.dateRange = [];
     this.trip = {
       name: '',
       startDate: new Date(0),
